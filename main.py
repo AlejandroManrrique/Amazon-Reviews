1357e72a0d5caeec3f0362641d16bd8ebc3963c2
Leidy <leidyhernandez.7@hotmail.com>
2023-10-11 21:20:45 -0500

    API

--- main.py
+++ main.py
@@ -2,11 +2,35 @@ from fastapi import FastAPI
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import re
+from sklearn.preprocessing import MinMaxScaler
 
 app = FastAPI()
 
 # Para leer el archivo parquet
 categories = pd.read_parquet('API/categories.parquet')
+
+# Crear un diccionario para buscar la categoría por asin
+categoria_por_asin = dict(zip(categories['asin'], categories['category']))
+
+# Cargar y preprocesar los datos una vez fuera de la función
+categorias = set(categories['category'])
+archivos_por_categoria = {}
+
+for categoria in categorias:
+    nombre_archivo = categoria + "_data.parquet"
+    archivo = pd.read_parquet(nombre_archivo)
+    encoded = pd.get_dummies(archivo, columns=['category'])
+    features = encoded.drop(['asin', 'overall', 'product'], axis=1)
+    scaler = MinMaxScaler()
+    features_scaled = scaler.fit_transform(features)
+    knn = NearestNeighbors(n_neighbors=6)
+    knn.fit(features_scaled)
+    archivos_por_categoria[categoria] = {
+        "archivo": archivo,
+        "encoded": encoded,
+        "knn": knn,
+        "features_scaled": features_scaled
+    }
 
 @app.get("/")
 def welcome():
@@ -14,54 +38,24 @@ def welcome():
             'def obtener_recomendaciones(producto_id : str)':'Dado un codigo de producto de Amazon (de las categorías Home_and_Kitchen, Arts_Crafts_and_Sewing, Appliances, AMAZON_FASHION y All_Beauty) devuelve una lista de recomendación de 5 productos de la misma categoría, de alta calificación y de precio similar.'}
 
 @app.get('/obtener_recomendaciones/{producto_id}')
-def obtener_recomendaciones(producto_id : str):
+def obtener_recomendaciones(producto_id):
     producto_id = producto_id.lower()
-    # Identificar la categoria del producto
-    categoria = None
-    for i in range(len(categories)):
-        if categories['asin'][i] == producto_id:
-            categoria = categories['category'][i]
-            break
-    
-    # Identificar el nombre del archivo y abrirlo
-    if categoria == None:
+    categoria = categoria_por_asin.get(producto_id)
+    if categoria is None:
         return "Producto no encontrado en el conjunto de datos."
-    
-    nombre_archivo = f"API/{categoria}_data.parquet"
-    archivo = pd.read_parquet(nombre_archivo)
 
-    # Convertir las columnas de texto (categoría) a números usando codificación one-hot
-    encoded = pd.get_dummies(archivo, columns=['category'])
+    archivo = archivos_por_categoria[categoria]["archivo"]
+    encoded = archivos_por_categoria[categoria]["encoded"]
+    features_scaled = archivos_por_categoria[categoria]["features_scaled"]
+    knn = archivos_por_categoria[categoria]["knn"]
 
-    # Seleccionar las características para el modelo (todas except 'asin' y 'overall')
-    features = encoded.drop(['asin', 'overall', 'product'], axis=1)
-
-    # Normalizar las características para asegurar que todas tengan el mismo peso
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler()
-    features_scaled = scaler.fit_transform(features)
-
-    # Inicializar el modelo de KNeighbors
-    knn = NearestNeighbors(n_neighbors=6)  # Se establece en 6 para obtener los 5 vecinos más cercanos (incluyendo el propio artículo)
-
-    # Ajustar el modelo
-    knn.fit(features_scaled)
-
-    # Verificar si el producto_id está presente en el DataFrame original
     if producto_id in encoded['asin'].values:
-        # Encuentra los vecinos más cercanos
         producto_idx = encoded[encoded['asin'] == producto_id].index[0]
         distancia, indice = knn.kneighbors(features_scaled[producto_idx].reshape(1, -1))
-
-        # Obtiene los índices de los vecinos (excluyendo el producto de referencia)
         indices_vecinos = indice.squeeze()[1:]
-
-        # Filtra los vecinos por mejor calificación y menor precio
         vecinos_filtrados = archivo.iloc[indices_vecinos].sort_values(by=['overall', 'price']).head(5)
         vecinos_filtrados = vecinos_filtrados.reset_index(drop=True)
         vecinos_filtrados.index = vecinos_filtrados.index + 1
-
-        for i in range(len(vecinos_filtrados)):
-            return vecinos_filtrados
+        return vecinos_filtrados
     else:
         return "Producto no encontrado en el conjunto de datos."
